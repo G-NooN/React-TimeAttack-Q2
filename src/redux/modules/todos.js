08841cc5f71497d2b3d2c6772bfad70ed7d5228f
GNooNJo <jjjchoon@nate.com>
2024-02-17 18:06:13 +0900

    feat: reducer 설정

--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -1,26 +1,68 @@
 // import uuid from "react-uuid";
 import shortid from "shortid";
 
+// 초기 state
 const initialState = [
   {
     id: shortid.generate(),
-    title: "",
-    body: "",
+    title: "제목1",
+    body: "내용1",
     isDone: false,
   },
+  {
+    id: shortid.generate(),
+    title: "제목2",
+    body: "내용2",
+    isDone: true,
+  },
 ];
+
+// action creators
+export const addTodo = (payload) => {
+  return {
+    type: "ADD_TODO",
+    payload,
+  };
+};
+export const deleteTodo = (payload) => {
+  return {
+    type: "DELETE_TODO",
+    payload,
+  };
+};
+export const switchTodo = (payload) => {
+  return {
+    type: "SWITCH_TODO",
+    payload,
+  };
+};
 
 // 리듀서
 const todos = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_TODO":
-      return; //TODO: 여기 작성
+      // action.payload : 새로운 todo item
+      const newTodoItem = action.payload;
+      return [...state, newTodoItem];
 
     case "DELETE_TODO":
-      return; //TODO: 여기 작성
+      // action.payload : 삭제할 todo item의 id
+      const deleteTargetId = action.payload;
+      const deletedTodoList = state.filter((todoItem) => todoItem.id !== deleteTargetId);
+      return deletedTodoList;
 
     case "SWITCH_TODO":
-      return; //TODO: 여기 작성
+      // action.payload : 변경할 todo item의 id
+      const switchTargetId = action.payload;
+      const switchedTodoList = state.map((todoItem) => {
+        if (todoItem.id === switchTargetId) {
+          const switchedTodoItem = { ...todoItem, isDone: !todoItem.isDone };
+          return switchedTodoItem;
+        } else {
+          return todoItem;
+        }
+      });
+      return switchedTodoList;
 
     default:
       return state;
