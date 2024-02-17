9f77181db43ba886c033e670fe99d9a7ab3ff533
GNooNJo <jjjchoon@nate.com>
2024-02-17 18:03:37 +0900

    feat: add common styled-components

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+// Section Title
+const StSectionTitle = styled.h2`
+  margin-left: 5px;
+`;
+
+// Item Info Title
+const StTitle = styled.span`
+  font-weight: bold;
+  margin-left: 5px;
+`;
+
+const StButton = styled.button`
+  margin: 0 3px;
+`;
+
+export { StSectionTitle, StTitle, StButton };
